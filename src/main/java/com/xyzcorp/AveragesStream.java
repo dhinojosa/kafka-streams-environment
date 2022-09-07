249d182594c2ab282513865576157b6635424ced
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-09-07 16:52:04 +0000

    Committed Streams and pom.xml with docker-compose.xml

--- src/main/java/com/xyzcorp/AveragesStream.java
+++ src/main/java/com/xyzcorp/AveragesStream.java
@@ -11,40 +11,50 @@ import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
 
 public class AveragesStream {
-    public static void main(String[] args) {
-        Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG,
-                "order-averages-stream");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
-                Serdes.String().getClass());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
-                Serdes.Integer().getClass());
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+//     public static void main(String[] args) {
+//         Properties props = new Properties();
+//         props.put(StreamsConfig.APPLICATION_ID_CONFIG,
+//                 "order-averages-stream");
+//         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
+//                 "localhost:9092");
+//         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
+//                 Serdes.String().getClass());
+//         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
+//                 Serdes.Integer().getClass());
+//         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        StreamsBuilder builder = new StreamsBuilder();
+//         StreamsBuilder builder = new StreamsBuilder();
 
-        KTable<String, Integer> totalRevenueByState = builder.table("total-revenue-by-state",
-                Consumed.with(Serdes.String(), Serdes.Integer())); // Key: State, Value: Amount
-                
-        KTable<String, Long> numberOrdersByState = builder.table("number-orders-by-state",
-                Consumed.with(Serdes.String(), Serdes.Long()));
-        /// ???
+//         KTable<String, Integer> totalRevenueByState = builder.table("total-revenue-by-state",
+//                 Consumed.with(Serdes.String(), Serdes.Integer())); // Key: State, Value: Amount
 
-        Topology topology = builder.build();
+//         KTable<String, Long> numberOrdersByState = builder.table("number-orders-by-state",
+//                 Consumed.with(Serdes.String(), Serdes.Long()));
 
-        KafkaStreams streams = new KafkaStreams(topology, props);
+//         KTable<String, Long> averagesTable = totalRevenueByState.leftJoin(numberOrdersByState,
+//                 (revenue, total) -> revenue / total);
 
-        streams.setUncaughtExceptionHandler(exception -> {
-            exception.printStackTrace();
-            return StreamThreadExceptionResponse.SHUTDOWN_CLIENT;
-        });
+//         averagesTable
+//                 .toStream()
+//                 .peek((key, value) -> System.out.printf("Placing key: %s, value %d in order-averages-by-state\n", key,
+//                         value))
+//                 .to("order-averages-by-state", Produced.with(Serdes.String(), Serdes.Long()));
 
-        streams.start();
+//         Topology topology = builder.build();
+//         System.out.println(topology.describe());
 
-        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-    }
+//         KafkaStreams streams = new KafkaStreams(topology, props);
+
+//         streams.setUncaughtExceptionHandler(exception -> {
+//             exception.printStackTrace();
+//             return StreamThreadExceptionResponse.SHUTDOWN_CLIENT;
+//         });
+
+//         streams.start();
+
+//         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+//     }
 }
--- src/main/java/com/xyzcorp/MyStreams.java
+++ src/main/java/com/xyzcorp/MyStreams.java
@@ -20,7 +20,7 @@ public class MyStreams {
         props.put(StreamsConfig.APPLICATION_ID_CONFIG,
                 "my-orders-stream");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
+                "broker:29092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
                 Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
